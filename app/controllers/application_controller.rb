842ff55a5cb929db59e009a8af3e770c15f25603
Jeff Sackett <Jeff.Sackett@Pearson.com>
2013-02-01 13:01:24 -0600

    force https TODO: sandwich creation interface, order completion, mailer with snappy little quote

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_https
+
+  def redirect_https
+    redirect_to :protocol => "https://" unless request.ssl?
+    return true
+  end
+
 end
